604d88563a347173e906a23a44f6981c8eb321c9
ESDRAS TECHNOLOGIES LTD <esdrastechnologiesinc@gmail.com>
2025-12-03 09:32:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // Replace with your Firebase config
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  // Your config here
 };
 
 initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
