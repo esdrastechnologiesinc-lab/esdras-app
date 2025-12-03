f96e520054887123f39838c1db05d58490cba709
ESDRAS TECHNOLOGIES LTD <esdrastechnologiesinc@gmail.com>
2025-12-03 09:44:45 +0100

    Create scan.jsx

---
+++ src/components/scan.jsx
@@ -0,0 +1,3 @@
+export default function Scan() {
+  return <div style={{ textAlign: 'center', marginTop: '5rem' }}><h2>360° Head Scan Coming Soon</h2><p>(Web version uses photo upload)</p></div>;
+}
